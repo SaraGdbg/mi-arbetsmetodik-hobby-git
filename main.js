abe75fe239920c0c5c35a350105f43fd6a0cb108
Sara Gdbg <146811766+SaraGdbg@users.noreply.github.com>
2024-03-28 15:48:10 +0100

    :sparkles: Add slideshow to start-page

--- main.js
+++ main.js
@@ -18,3 +18,18 @@ joinBtn.addEventListener("click", function () {
   });
 });
 console.log(joinBtn);
+
+/**
+ * Shows images in slideshow at front page
+ */
+let currentIndex = 0;
+const images = document.querySelectorAll("#slideshow img");
+
+function showNextImage() {
+  images[currentIndex].style.display = "none";
+  currentIndex = (currentIndex + 1) % images.length;
+  images[currentIndex].style.display = "block";
+}
+
+images[currentIndex].style.display = "block";
+setInterval(showNextImage, 3000);
