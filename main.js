1037066293bb353bf240f69f9dd63e35c12b01ec
Sara Gdbg <146811766+SaraGdbg@users.noreply.github.com>
2024-03-28 12:52:10 +0100

    :sparkles: Add function to joinBtn

--- main.js
+++ main.js
@@ -5,4 +5,16 @@ const patternsBtn = document.getElementById("patternsBtn");
 patternsBtn.addEventListener("click", function () {
   window.open("https://www.ravelry.com/patterns/knitting", "_blank");
 });
-console.log(patternsBtn);
+
+/**
+ * Redirects user to contact info
+ */
+const joinBtn = document.getElementById("joinBtn");
+joinBtn.addEventListener("click", function () {
+  const footerPostition = document.getElementById("contactInfo").offsetTop;
+  window.scrollTo({
+    top: footerPostition,
+    behavior: "smooth",
+  });
+});
+console.log(joinBtn);
